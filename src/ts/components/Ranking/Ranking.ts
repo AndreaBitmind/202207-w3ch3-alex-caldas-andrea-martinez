4ed0a63e3b69c99db1ab0c700deea498a51d3b29
Andrea Martinez <andrea.maort@gmail.com>
2022-07-20 20:58:10 +0200

    ranking element created

--- src/ts/components/Ranking/Ranking.ts
+++ src/ts/components/Ranking/Ranking.ts
@@ -1,8 +1,14 @@
 class Ranking {
   constructor(childAmount: number) {
     const container = document.createElement("ul");
-    for (let i = 0; i < childAmount; i += 1) {
+    container.className = "score";
+    for (let i = 1; i <= childAmount; i += 1) {
       const child = document.createElement("li");
+      child.className = "score__star";
+      const icon = document.createElement("i");
+      icon.className = "icon-score far fa-star";
+      icon.title = `${i}/${childAmount}`;
+      child.appendChild(icon);
       container.appendChild(child);
     }
 
